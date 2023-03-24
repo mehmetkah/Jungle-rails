6189de9750849518382943ac7a00e56da671d3d8
“mehmetkah” <mehmetkahca@gmail.com>
2023-03-24 03:34:13 -0600

    user authentication

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,42 @@
-class ProductsController < ApplicationController
+class Admin::ProductsController < ApplicationController
+
+  before_action :authenticate
 
   def index
-    @products = Product.all.order(created_at: :desc)
+    @products = Product.order(id: :desc).all
   end
 
-  def show
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to [:admin, :products], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
     @product = Product.find params[:id]
+    @product.destroy
+    redirect_to [:admin, :products], notice: 'Product deleted!'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
+    )
   end
 
 end
