0ec87981465b3a64d2837c2518d22149a6d6f0b4
“mehmetkah” <mehmetkahca@gmail.com>
2023-03-24 01:44:17 -0600

    basic user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
-  protect_from_forgery with: :exception
 
+  protect_from_forgery with: :exception
+  
   private
+
+  def authenticate
+    http_basic_authenticate_or_request_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
+  end 
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
